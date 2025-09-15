4e7a94d646f0ad87532e5f9fbb9d672c0b155cdc
Luis Paredes <luisjosepr@gmail.com>
2025-09-15 13:23:00 -0300

    fix: pytest+fastAPI

---
+++ tests/test_hello.py
@@ -0,0 +1,24 @@
+import pytest
+from urllib.parse import urlencode
+from fastapi.testclient import TestClient
+from src.app import app
+
+client = TestClient(app)
+
+def test_answer():
+
+    query_params = {
+        "max_value" : True,
+        "include_ar": True
+    }
+
+    response = client.get(f"/data_analysis?{urlencode(query_params)}")
+
+    assert response.status_code == 200
+
+    # response_data = response.json()
+
+    # assert response_data == {"message": [product]}
+
+def test_assert():
+    assert True
